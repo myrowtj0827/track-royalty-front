9062a8950ec928110389b6b24f16ee7b087bbc9d
myrowtj0827 <mnblkjpoi0324@hotmail.com>
2020-12-17 13:42:22 -0500

    minor change

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-    // SIM_API_URL: "http://127.0.0.1:7000",
-    FRONT_URL: "http://localhost:3000",
-    SIM_API_URL: "https://gifted-galileo-95c683.netlify.app/",
+    SIM_API_URL: "http://127.0.0.1:7000",
+    //FRONT_URL: "http://localhost:3000",
+    FRONT_URL: "https://gifted-galileo-95c683.netlify.app/",
 };
