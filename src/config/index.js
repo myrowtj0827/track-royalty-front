3a70f772890d70994bb0d250986d655ea3a82e0a
myrowtj0827 <mnblkjpoi0324@hotmail.com>
2021-01-15 02:17:38 -0500

    sub domain push

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 export default {
     //SIM_API_URL: "http://127.0.0.1:7000",
-    FRONT_URL: "http://localhost:3000",
-    SIM_API_URL: "https://track-royalty-api.herokuapp.com",
-    // FRONT_URL: "https://gifted-galileo-95c683.netlify.app",
+    //FRONT_URL: "http://localhost:3000",
+    
+    SIM_API_URL: "http://royalties.jewishmusic.fm",
+    //SIM_API_URL: "https://track-royalty-api.herokuapp.com",
 };
