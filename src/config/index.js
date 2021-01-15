85f069fe5b27b588d7c92ef4c7c684550c30a2de
myrowtj0827 <mnblkjpoi0324@hotmail.com>
2021-01-15 12:19:16 -0500

    minor change

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,7 @@ export default {
     //SIM_API_URL: "http://127.0.0.1:7000",
     //FRONT_URL: "http://localhost:3000",
     
-    SIM_API_URL: "http://royalties.jewishmusic.fm",
+    SIM_API_URL: "http://167.86.96.120:7000",
+    //FRONT_URL: "http://royalties.jewishmusic.fm",    
     //SIM_API_URL: "https://track-royalty-api.herokuapp.com",
 };
